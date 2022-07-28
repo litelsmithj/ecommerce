b1d99b93ee4bd2be1caa3a2f36da0ecc48b0a059
Jake Litel-Smith <litelsmithj@gmail.com>
2022-07-28 07:10:17 -0700

    Products on homepage from Sanity

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,9 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
+import { urlFor } from '../lib/client'
 
-const Product = () => {
+const Product = ({product: {name, price, slug, image}}) => {
   return (
     <div>
-      
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'>
+          <img 
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className='product-image'
+          ></img>
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>${price}</p>
+        </div>
+      </Link>
     </div>
   )
 }
