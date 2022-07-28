6f4d30627e0501a0bb69461cb1ed2bb007b7a39f
Jake Litel-Smith <litelsmithj@gmail.com>
2022-07-28 07:55:13 -0700

    Add header and footer for easier navigation

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
+import {AiOutlineShopping} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">
+          JLS Ecommerce
+        </Link>
+      </p>
+
+      <button type='button' className='cart-icon' onClick="">
+        <AiOutlineShopping/>
+        <span className='cart-item-qty'>1</span>
+      </button>
+
     </div>
   )
 }
