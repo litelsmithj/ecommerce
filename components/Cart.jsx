4ee6ccfc36c43b1dd0b0b91a573630548cbc62a4
Jake Litel-Smith <litelsmithj@gmail.com>
2022-08-02 08:33:43 -0700

    Add stripe integration with test account

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,12 +1,37 @@
 import React, { useRef } from 'react'
+import toast from 'react-hot-toast'
 import { AiOutlineLeft, AiOutlineShopping, AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 import {TiDeleteOutline} from 'react-icons/ti'
 import { useStateContext } from '../context/StateContext'
 import { urlFor } from '../lib/client'
+import getStripe from '../lib/getStripe'
 
 const Cart = () => {
   const cartRef = useRef()
   const {totalQuantities, totalPrice, cartItems, setShowCart, toggleCartItemQuantity, onRemove} = useStateContext()
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe()
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems)
+    })
+
+    if(response.statusCode === 500) return;
+
+    const data = await response.json()
+
+    console.log(data)
+
+    toast.loading('Redirecting...')
+
+    stripe.redirectToCheckout({sessionId: data.id})
+  }
+
   return (
     <div className='cart-wrapper' ref={cartRef}>
       <div className='cart-container'>
@@ -64,7 +89,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className='btn-container'>
-              <button type='button' className='btn'>
+              <button type='button' className='btn' onClick={handleCheckout}>
                 Pay with Stripe
               </button>
             </div>
